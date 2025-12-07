834584a3bc87dac3d483be97f30d37281147df92
ThanawatU <149835326+ThanawatU@users.noreply.github.com>
2025-12-07 12:39:03 +0000

    Make Quick sort in Alogrithms.py and make simple I/O in main.py

--- project/Algorithms.py
+++ project/Algorithms.py
@@ -1,2 +1,19 @@
-class Algorithms:
-    
+class Algorithms_sort:
+    def quick_sort(self, input, reverse=False):
+        """
+        input: takes number array input
+        reverse: takes Boolean 
+        """
+        if len(input) <= 1:
+            return input 
+
+        pivot = input[len(input) // 2]
+        
+        less = [x for x in input if x < pivot]
+        equal = [x for x in input if x == pivot]
+        greater = [x for x in input if x > pivot]
+
+        if reverse == True:
+            return self.quick_sort(greater, reverse=True) + equal  + self.quick_sort(less, reverse=True)
+            
+        return self.quick_sort(less) + equal  + self.quick_sort(greater)
