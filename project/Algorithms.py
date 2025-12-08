1092088736ea29824e9e413cd01fd488c79571c6
Kanyapat <kanyapat.c@kkumail.com>
2025-12-09 00:54:33 +0700

    add bubble sort algorithm in Algorithms.py

--- project/Algorithms.py
+++ project/Algorithms.py
@@ -17,3 +17,17 @@ class Algorithms_sort:
             return self.quick_sort(greater, reverse=True) + equal  + self.quick_sort(less, reverse=True)
             
         return self.quick_sort(less) + equal  + self.quick_sort(greater)
+    
+    def bubble_sort(self, input, reverse=False):
+        """
+        input: takes number array input
+        reverse: takes Boolean 
+        """
+        
+        n = len(input)
+        
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if (input[j] > input[j+1] and not reverse) or (input[j] < input[j+1] and reverse):
+                    input[j], input[j+1] = input[j+1], input[j]
+        return input
